9029dba768d31ad5ec6f960b8c289c2c2f6acdca
sasibhagya <bhagyapalli12@gmail.com>
2023-11-11 08:58:55 +0530

    5th commit

--- conditionals.py
+++
@@ -1,9 +0,0 @@
-name=input("enter your name : ")
-if name=="Bhagya" or "bhagya":
-    print("yes , you are in python class")
-elif name=="sasi" or "Sasi":
-    print("yes , you are in python class")
-elif name=="Dharani" or "dharani":
-    print("yes , you are in python class")
-else:
-    print("your name is not registered in python class")
