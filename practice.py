082d5b66d40de83f4dd9f34398520a97e53db459
sasibhagya <bhagyapalli12@gmail.com>
2023-11-11 08:30:03 +0530

    first commit

---
+++ practice.py
@@ -0,0 +1,16 @@
+a=[10,20,30,40,50]
+b=[10,20,30,40,50]
+c=[x*y for x,y in zip(a,b)]
+print(c)
+
+
+a=[10,20,30,40,50]
+b=[10,20,30,40]
+c=[x*y for x,y in zip(a,b)]
+print(c)
+
+
+
+
+a="my name is bhagya"
+print(a[10:3:-1])
