7e6a1838953062caefe666277788b45d4f5f4e75
sasibhagya <bhagyapalli12@gmail.com>
2023-11-14 00:08:09 +0530

    patterns

---
+++ nestedloop patterns.py
@@ -0,0 +1,286 @@
+'''num=5
+for i in range(num):
+    for j in range(i + 1):
+        print("*", end="")
+    print()
+print()
+'
+
+num=5
+for i in range(num):
+    for j in range(i + 1):
+        print(i, end="")
+    print()
+print()
+
+
+num=5
+for i in range(num):
+    for j in range(i + 1):
+        print(j, end="")
+    print()
+print()
+
+
+num = 5
+for i in range(num,0,-1):
+    for j in range(i):
+        print("*", end="")
+    print()
+print()
+
+
+num = 5
+for i in range(num,0,-1):
+    for j in range(i):
+        print(i, end="")
+    print()
+print()
+
+
+num = 5
+for i in range(num,0,-1):
+    for j in range(i):
+        print(j, end="")
+    print()
+print()
+
+
+num=4
+for i in range(num):
+    for j in range(num):
+        print("*","",end="")
+    print()
+print()
+
+num=4
+for i in range(num):
+    for j in range(num):
+        print(i,"",end="")
+    print()
+print()
+
+num=4
+for i in range(num):
+    for j in range(num):
+        print(j,"",end="")
+    print()
+print()
+
+
+num=10
+for i in range(num-5):
+    for j in range(num):
+        print("*","",end="")
+    print()
+print()
+
+num=10
+for i in range(num-5):
+    for j in range(num):
+        print(i,"",end="")
+    print()
+print()
+
+num=10
+for i in range(num-5):
+    for j in range(num):
+        print(j,"",end="")
+    print()
+print()
+
+num=10
+for i in range(num):
+    for j in range(num-5):
+        print("*","",end="")
+    print()
+print()
+
+num=10
+for i in range(num):
+    for j in range(num-5):
+        print(i,"",end="")
+    print()
+print()
+
+num=10
+for i in range(num):
+    for j in range(num-5):
+        print(j,"",end="")
+    print()
+print()'''
+
+'''
+n=5
+for i in range(n):   
+    for j in range(n - i - 1):
+        print(" ", end="")
+    for k in range(i + 1):
+        print("*", end="")
+    print()
+print() 
+
+n=5
+for i in range(n):   
+    for j in range(n - i - 1):
+        print(" ", end="")
+    for k in range(i + 1):
+        print(i, end="")
+    print()
+print()
+
+
+n=5
+for i in range(n):   
+    for j in range(n - i - 1):
+        print(" ", end="")
+    for k in range(i + 1):
+        print(k, end="")
+    print()
+print()  '''
+'''
+num=5
+for i in range(num - 1):
+    for j in range(i + 1):
+        print(" ", end="")
+    for j in range(num - i - 1):
+        print("*", end="")
+    print()
+print()
+
+num=5
+for i in range(num - 1):
+    for j in range(i + 1):
+        print(" ", end="")
+    for j in range(num - i - 1):
+        print(i, end="")
+    print()
+print()
+
+num=5
+for i in range(num - 1):
+    for j in range(i + 1):
+        print(" ", end="")
+    for j in range(num - i - 1):
+        print(j, end="")
+    print()
+print()'''
+
+'''num = 5
+for i in range(num):
+    for j in range(num - i - 1):
+        print(" ", end="")
+    for j in range(i + 1):
+        print("*", end="")
+    for j in range(i):
+        print("*", end="")
+    print()
+
+
+num = 5
+for i in range(num):
+    for j in range(num - i - 1):
+        print(" ", end="")
+    for j in range(i + 1):
+        print(i, end="")
+    for j in range(i):
+        print(i, end="")
+    print()
+
+num = 5
+for i in range(num):
+    for j in range(num - i - 1):
+        print(" ", end="")
+    for j in range(i + 1):
+        print(j, end="")
+    for j in range(i):
+        print(j, end="")
+    print()
+print()
+
+
+
+num=6
+for i in range(num-1):
+    for j in range(i + 1):
+        print(" ", end="")
+    for j in range(num - i - 1):
+        print("*", end="")
+    for j in range(num - i - 2):
+        print("*", end="")
+    print()
+
+num=6
+for i in range(num - 1):
+    for j in range(i + 1):
+        print(" ", end="")
+    for j in range(num - i - 1):
+        print(i, end="")
+    for j in range(num - i - 2):
+        print(i, end="")
+    print()
+
+num=6
+for i in range(num - 1):
+    for j in range(i + 1):
+        print(" ", end="")
+    for j in range(num - i - 1):
+        print(j, end="")
+    for j in range(num - i - 2):
+        print(j, end="")
+    print()'''
+
+
+num=5
+for i in range(num):
+    for j in range(num - i - 1):
+        print(" ", end="")
+    for j in range(i + 1):
+        print("*", end="")
+    for j in range(i):
+        print("*", end="")
+    print()
+for i in range(num - 1):
+    for j in range(i + 1):
+        print(" ", end="")
+    for j in range(num - i - 1):
+        print("*", end="")
+    for j in range(num - i - 2):
+        print("*", end="")
+    print()
+
+num=5
+for i in range(num):
+    for j in range(num - i - 1):
+        print(" ", end="")
+    for j in range(i + 1):
+        print(i, end="")
+    for j in range(i):
+        print(i, end="")
+    print()
+for i in range(num - 1):
+    for j in range(i + 1):
+        print(" ", end="")
+    for j in range(num - i - 1):
+        print(i, end="")
+    for j in range(num - i - 2):
+        print(i, end="")
+    print()
+
+num=5
+for i in range(num):
+    for j in range(num - i - 1):
+        print(" ", end="")
+    for j in range(i + 1):
+        print(j, end="")
+    for j in range(i):
+        print(j, end="")
+    print()
+for i in range(num - 1):
+    for j in range(i + 1):
+        print(" ", end="")
+    for j in range(num - i - 1):
+        print(j, end="")
+    for j in range(num - i - 2):
+        print(j, end="")
+    print()
