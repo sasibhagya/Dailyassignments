1392309391114b9ddfc61fe38313fd60188140ba
sasibhagya <bhagyapalli12@gmail.com>
2023-11-23 23:14:42 +0530

    committed

---
+++ 24 11 23 assignment tuple.py
@@ -0,0 +1,25 @@
+# multiply the tuple with 2
+l=(10,20,30,40,50)
+print(l*2)
+
+#types of tuple we can write
+t=(10,20,30,40)
+print(type(t))
+
+t=10,20,30,40
+print(type(t))
+
+t=10
+print(type(t))
+
+t=10,
+print(type(t))
+
+t=()
+print(type(t))
+
+t=(10)
+print(type(t))
+
+t=(10,)
+print(type(t))
