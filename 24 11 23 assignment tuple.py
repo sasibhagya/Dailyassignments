b5648e4391b456995ab721a25575b67684206be7
sasibhagya <bhagyapalli12@gmail.com>
2023-11-27 14:36:54 +0530

    committed

--- 24 11 23 assignment tuple.py
+++ 24 11 23 assignment tuple.py
@@ -1,3 +1,10 @@
+#write a program to display unique vowels present in the given words
+word="my name is bhagya iam learning python course in marolix solutions"
+for i in word:
+    if i in "aeiou":
+        print(i,"",end="")
+print()
+
 # multiply the tuple with 2
 l=(10,20,30,40,50)
 print(l*2)
