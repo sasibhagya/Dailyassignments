07a556080a6bf627fcdd5f2729cb98ab6432a897
sasibhagya <bhagyapalli12@gmail.com>
2023-11-28 22:01:03 +0530

    committed

---
+++ 28 11 23 assignment tuple2.py
@@ -0,0 +1,15 @@
+#write a pro to take tuple of numbers from keyboard and print Sum,avg
+t=(10,20,30,40,50)
+sum=0
+for i in t:
+    sum=sum+i
+print(sum)
+
+t=(10,20,30,40,50,60)
+sum=0
+avg=len(t)
+for i in t:
+    sum=sum+i
+print(sum/avg)
+
+    
